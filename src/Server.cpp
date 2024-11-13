e3c4aedda1fc4e9866083f15542424c7b3b6788f
mapleinred <53573954+mapleinred@users.noreply.github.com>
2024-11-13 18:58:38 +0800

    Initial commit

--- src/Server.cpp
+++ src/Server.cpp
@@ -87,4 +87,68 @@ void Server::run()
 void Server::handleconnections()
 {
     struct epoll_event events[MAXEVENTS];
+    while (1)
+    {
+        int eventcount = epoll_wait(epollfd, events, MAXEVENTS, -1);
+        if (eventcount < 0)
+        {
+            perror("epoll_wait failed");
+            break;
+        }
+        for (int i = 0; i < eventcount; ++i)
+        {
+            if (events[i].data.fd == serversocket)
+            {
+                int clientsocket = accept(serversocket, NULL, NULL);
+                if (clientsocket >= 0)
+                {
+                    fcntl(clientsocket, F_SETFL, O_NONBLOCK);
+                    struct epoll_event ev;
+                    ev.events = EPOLLIN;
+                    ev.data.fd = clientsocket;
+                    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, clientsocket, &ev) < 0)
+                    {
+                        perror("epoll_ctl failed");
+                        close(clientsocket);
+                    }
+                }
+            }
+            else
+            {
+                handlerequest(events[i].data.fd);
+            }
+            if (epoll_ctl(epollfd, EPOLL_CTL_DEL, events[i].data.fd, NULL) < 0)
+            {
+                perror("epoll_ctl failed");
+            }
+            close(events[i].data.fd);
+        }
+    }
+}
+
+
+void Server::handlerequest(int clientsocket)
+{
+    char buffer[1024];
+    int bytesread = read(clientsocket, buffer, 1024);
+    if (bytesread < 0)
+    {
+        perror("read failed");
+        close(clientsocket);
+        return ;
+    }
+    if (bytesread == 0)
+    {
+        close(clientsocket);
+        return ;
+    }
+    std::string request(buffer, bytesread);
+    std::cout << request << std::endl;
+    std::string response = "HTTP/1.1 200 OK\nContent-Length: 12\n\nHello world!";
+    int byteswritten = write(clientsocket, response.c_str(), response.size());
+    if (byteswritten != (int)response.size())
+    {
+        perror("write failed");
+    }
+    close();
 }
