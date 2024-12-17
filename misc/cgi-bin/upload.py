bbe9385eee929e58d06c7f6cbc81780926694417
mapleinred <53573954+mapleinred@users.noreply.github.com>
2024-12-17 18:05:55 +0800

    upload.py

---
+++ misc/cgi-bin/upload.py
@@ -0,0 +1,15 @@
+-----------------------------140852920241981611193399813780
+Content-Disposition: form-data; name="uploaded_file"; filename="time.py"
+Content-Type: text/x-python
+
+#!/usr/bin/python3
+
+import datetime
+import cgi
+
+print("HTTP/1.1 200 OK")
+print("Content-type: text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print(datetime.datetime.strftime(datetime.datetime.now(), "<h1>  %H:%M:%S </h1>"))
+-----------------------------140852920241981611193399813780--
